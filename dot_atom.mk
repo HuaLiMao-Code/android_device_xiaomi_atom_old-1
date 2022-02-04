b22b7f12124183f7d896af2dc6d5c202a3cd6c1a
HuaLiMao-AQ <hualimao_aq@163.com>
2022-02-04 12:33:41 +0800

    atom:ify: ArrowOS-11 >> DotOS-11 Signed-off-by: HuaLiMao-AQ <hualimao_aq@163.com>

--- dot_atom.mk
+++ dot_atom.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022 The ArrowOS Project
+# Copyright (C) 2022 The DotOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,11 +20,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from atom device
 $(call inherit-product, device/xiaomi/atom/device.mk)
 
-# Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common DotOS stuff.
+$(call inherit-product, vendor/dot/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_atom
+PRODUCT_NAME := dot_atom
 PRODUCT_DEVICE := atom
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := M2004J7AC
